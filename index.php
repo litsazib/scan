62174e5d1ffb640585ece1138a42125010a99ce0
litsazib <roktimsazib92@gmail.com>
2016-11-17 17:58:47 +0600

    hover icon

--- index.php
+++ index.php
@@ -44,40 +44,14 @@
         </li>
         
         <li class="what-hosting">
-          <a href="web-hosting/linux-hosting" class="text-link">Web Hosting</a>
+          <a href="web-hosting/linux-hosting" class="text-link">E-Commerce</a>
           <a href="web-hosting/linux-hosting">
           <div class="dot-bg">
             <img src="images/whatwedo/web-hosting.jpg">
           </div>
             <div class="svg-icon">
-              <span class="gradient"></span>
-              <svg version="1.1" id="hosting-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 99.213 99.213" style="enable-background:new 0 0 99.213 99.213;" xml:space="preserve">
-              <circle class="st1" cx="49.606" cy="49.606" r="49.606"/>
-                <circle class="st3" cx="49.606" cy="49.606" r="49.606"/>
-              <g>
-                <path class="st0" d="M27.446,68.937h15.009v-2.527H27.446V68.937L27.446,68.937z M39.378,67.321h2.055v0.705h-2.055V67.321
-                  L39.378,67.321z M34.427,47.601c0-0.451,0.37-0.818,0.824-0.818c0.456,0,0.826,0.367,0.826,0.818c0,0.451-0.37,0.818-0.826,0.818
-                  C34.797,48.419,34.427,48.052,34.427,47.601L34.427,47.601z M39.783,47.601c0-0.451,0.37-0.818,0.824-0.818
-                  c0.456,0,0.826,0.367,0.826,0.818c0,0.451-0.372,0.818-0.826,0.818S39.783,48.052,39.783,47.601L39.783,47.601z M27.446,62.292
-                  h15.009v-2.527H27.446V62.292L27.446,62.292z M39.378,60.675h2.055v0.707h-2.055V60.675L39.378,60.675z M24.107,73.11h21.462
-                  V43.264H24.107V73.11L24.107,73.11z M40.607,45.446c1.201,0,2.176,0.967,2.176,2.155c0,1.188-0.977,2.156-2.176,2.156
-                  s-2.176-0.967-2.176-2.156C38.432,46.413,39.407,45.446,40.607,45.446L40.607,45.446z M35.251,45.446
-                  c1.201,0,2.176,0.967,2.176,2.155c0,1.188-0.975,2.156-2.176,2.156c-1.199,0-2.176-0.967-2.176-2.156
-                  C33.075,46.413,34.052,45.446,35.251,45.446L35.251,45.446z M26.096,51.69h17.71v5.203h-17.71V51.69L26.096,51.69z M26.096,58.427
-                  h17.71v5.203h-17.71V58.427L26.096,58.427z M26.096,65.071h17.71v5.203h-17.71V65.071L26.096,65.071z M27.446,55.556h15.009v-2.527
-                  H27.446V55.556L27.446,55.556z M39.378,53.847h2.055v0.705h-2.055V53.847L39.378,53.847z M75.104,48.493
-                  c0,1.309-0.121,2.59-0.338,3.84l-0.396,1.822c-2.537,9.613-11.379,16.729-21.867,16.729c-1.332,0-2.636-0.123-3.904-0.342v-13.34
-                  c0.596,0.248,1.075,0.672,1.612,1.09c0.791,0.617,1.993,0.113,2.915,0.143c2.313,0.076,2.711,2.549,2.871,4.338
-                  c0.117,1.313,1.342,2.396,1.004,3.768c-0.15,0.615-0.475,1.234-0.992,1.615c3.676-0.643,7.01-2.271,9.713-4.604
-                  c-0.938-2.322-3.635-3.738-3.398-6.793c0.553-5.203,6.607-2.973,10.018-4.744c0.207-1.145,0.32-2.32,0.32-3.522
-                  c0-7.475-4.17-14.001-10.326-17.424c0.1,0.093,0.203,0.185,0.293,0.275c3.971,3.932,1.832,4.235,2.443,8.775
-                  c0,1.512,3.156,1.26,2.852,3.983c-0.611,2.725-0.713,5.397-3.463,3.126c-2.748-2.269-4.58-0.453-5.496-0.756
-                  c-1.223-0.605-1.834-3.329-3.361-3.329c-4.275,0.302-1.525,8.775-6.718,6.355c-0.104-0.053-0.193-0.113-0.288-0.172v-8.966h-2.888
-                  c-0.1-3.431-0.372-6.946-3.372-9.102c-3.615,2.1-6.495,5.29-8.221,9.102H31.45c3.292-8.336,11.486-14.259,21.051-14.259
-                  C64.966,26.103,75.104,36.146,75.104,48.493L75.104,48.493z"/>
-              </g>
-              </svg>
-            </div> <!-- /.svg-icon -->
+              <img onmouseout="this.src='images/icon/e-commerce.jpg'" onmouseover="this.src='images/icon/e-commerce_hover.jpg'" onclick="return submitsearch(document.frmsearch);" src="images/icon/e-commerce.jpg" alt="">
+            </div> 
           </a>            
         </li>
         
@@ -127,32 +101,14 @@
         </li>
         
         <li class="what-vps">
-          <a href="vps-hosting/linux-vps" class="text-link">VPS</a> 
+          <a href="vps-hosting/linux-vps" class="text-link">Mobile Apps</a> 
           <a href="vps-hosting/linux-vps">
           <div class="dot-bg">
             <img src="images/whatwedo/vps.jpg">
           </div>
             <div class="svg-icon">
-              <span class="gradient"></span>
-              <svg version="1.1" id="vps-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 99.213 99.213" style="enable-background:new 0 0 99.213 99.213;" xml:space="preserve">
-              <circle class="st1" cx="49.606" cy="49.606" r="49.606"/>
-                <circle class="st3" cx="49.606" cy="49.606" r="49.606"/>
-              <path class="st0" d="M41.42,50.687c-0.916,0-1.66-0.744-1.66-1.66c0-0.917,0.744-1.66,1.66-1.66s1.659,0.742,1.659,1.66
-                C43.079,49.943,42.336,50.687,41.42,50.687L41.42,50.687z M65.996,69.526h-0.551c-0.291,2.574-2.48,4.581-5.131,4.581H38.9
-                c-2.65,0-4.839-2.007-5.13-4.581h-0.554c-8.882,0-16.109-7.227-16.109-16.111c0-8.76,7.029-15.911,15.743-16.105
-                c1.757-7.087,8.143-12.203,15.634-12.203c4.072,0,7.959,1.522,10.945,4.289c2.346,2.173,3.973,4.958,4.711,8.016
-                c0.615-0.072,1.234-0.107,1.857-0.107c8.881,0,16.109,7.227,16.107,16.11C82.105,62.299,74.879,69.526,65.996,69.526L65.996,69.526z
-                 M62.455,47.428c0-1.183-0.957-2.143-2.141-2.143H38.9c-1.183,0-2.141,0.96-2.141,2.143v3.197c0,1.183,0.958,2.141,2.141,2.141
-                h21.414c1.184,0,2.141-0.958,2.141-2.141V47.428L62.455,47.428z M62.455,56.585c0-1.183-0.957-2.141-2.141-2.141H38.9
-                c-1.183,0-2.141,0.958-2.141,2.141v3.197c0,1.183,0.958,2.143,2.141,2.143h21.414c1.184,0,2.141-0.96,2.141-2.143V56.585
-                L62.455,56.585z M62.455,65.743c0-1.182-0.957-2.141-2.141-2.141H38.9c-1.183,0-2.141,0.959-2.141,2.141v3.198
-                c0,1.182,0.958,2.142,2.141,2.142h21.414c1.184,0,2.141-0.96,2.141-2.142V65.743L62.455,65.743z M41.42,69.003
-                c-0.916,0-1.66-0.744-1.66-1.66c0-0.918,0.744-1.66,1.66-1.66s1.659,0.742,1.659,1.66C43.079,68.259,42.336,69.003,41.42,69.003
-                L41.42,69.003z M41.42,59.844c-0.916,0-1.66-0.744-1.66-1.66s0.744-1.659,1.66-1.659s1.659,0.743,1.659,1.659
-                S42.336,59.844,41.42,59.844L41.42,59.844z"/>
-              </svg>
-
-            </div> <!-- /.svg-icon -->
+          <img onmouseout="this.src='images/icon/bootstrap-icon.jpg'" onmouseover="this.src='images/icon/bootstrap-icon_hover.jpg'" onclick="return submitsearch(document.frmsearch);" src="images/icon/bootstrap-icon.jpg" alt="">      
+            </div> 
           </a>            
         </li>
         
@@ -187,30 +143,13 @@
         </li>
         
         <li class="what-support">
-          <a href="contact-us" class="text-link">Support</a>  
+          <a href="contact-us" class="text-link">SEO</a>  
           <a href="contact-us">
           <div class="dot-bg">  
           <img src="images/whatwedo/support.jpg">
           </div>
             <div class="svg-icon">
-              <span class="gradient"></span>
-              <svg version="1.1" id="support-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 99.213 99.213" style="enable-background:new 0 0 99.213 99.213;" xml:space="preserve">
-              <circle class="st1" cx="49.606" cy="49.606" r="49.606"/>
-                <circle class="st3" cx="49.606" cy="49.606" r="49.606"/>
-                <g>
-                 <path class="st0" d="M67.474,67.877l-2.936,2.924c-0.525,0.521-2.061,0.849-2.107,0.849
-                c-9.295,0.076-18.246-3.556-24.822-10.103c-6.597-6.563-10.251-15.503-10.15-24.781c0-0.006,0.336-1.491,0.86-2.007l2.936-2.926
-                c1.078-1.071,3.141-1.555,4.586-1.076l0.619,0.206c1.445,0.48,2.955,2.066,3.356,3.529l1.479,5.399
-                c0.401,1.463-0.138,3.547-1.216,4.619l-1.962,1.952c1.925,7.104,7.521,12.669,14.651,14.587l1.965-1.952
-                c1.076-1.072,3.174-1.607,4.645-1.209l5.426,1.475c1.467,0.396,3.063,1.897,3.545,3.336l0.205,0.617
-                C69.037,64.754,68.55,66.808,67.474,67.877L67.474,67.877z M45.686,43.959v-2.28l2.096-1.888
-                c3.539-3.149,5.258-4.959,5.307-6.846c0-1.313-0.797-2.358-2.666-2.358c-1.398,0-2.618,0.694-3.466,1.341l-1.071-2.702
-                c1.222-0.919,3.115-1.662,5.31-1.662c3.664,0,5.683,2.13,5.683,5.059c0,2.707-1.969,4.865-4.312,6.947l-1.5,1.237v0.051h6.109
-                v3.102L45.686,43.959L45.686,43.959z M66.222,43.959v-3.848h-7.178v-2.453l6.131-9.821h4.637v9.449h1.943v2.825h-1.943v3.848
-                H66.222L66.222,43.959L66.222,43.959z M66.222,37.287v-3.573c0-0.967,0.049-1.958,0.123-3.003h-0.1
-                c-0.521,1.045-0.947,1.986-1.496,3.003l-2.166,3.522v0.051H66.222L66.222,37.287z"/>
-                </g>
-              </svg>
+        <img onmouseout="this.src='images/icon/seo.jpg'" onmouseover="this.src='images/icon/seo_hover.jpg'" onclick="return submitsearch(document.frmsearch);" src="images/icon/seo.jpg" alt="">       
             </div> 
           </a>
         </li>        
@@ -361,7 +300,8 @@
                         <a href="coldFusion-application-maintenance-service.html" title="ColdFusion Application Maintenance Service">ColdFusion Application Maintenance Service</a></p>
                       </strong>
                       <span>
-                        <a title="ColdFusion Application Maintenance Service"><img onmouseout="this.src='images/icon/word-press-icon.png'" onmouseover="this.src='images/icon/word-press-icon-hover.png'" onclick="return submitsearch(document.frmsearch);" src="images/icon/word-press-icon.png" alt=""></a>
+                        <a title="ColdFusion Application Maintenance Service">
+                        <img onmouseout="this.src='images/icon/word-press-icon.png'" onmouseover="this.src='images/icon/word-press-icon-hover.png'" onclick="return submitsearch(document.frmsearch);" src="images/icon/word-press-icon.png" alt=""></a>
                       </span>
                     </li>
                   </ul>
