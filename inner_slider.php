f6c22a5270d1b3a7aab9dba30b4aa390df845fe1
litsazib <roktimsazib92@gmail.com>
2016-11-19 16:02:01 +0600

    Inner slider solid bg added

--- inner_slider.php
+++ inner_slider.php
@@ -1,26 +1,32 @@
 <div class="slidermar sl_color">
 <div class="master-slider " id="masterslider">    
-    <div class="ms-slide slide-1" data-delay="2">    
-        <div class="slide-pattern"></div>        
-        <img src="js/masterslider/site-img31.jpg" data-src="images/sliders/master/site-img31.jpg" alt="" />		
-		<img class="ms-layer" alt="bootstrap conversion" style="top:150px; right:80px"
+    <div class="ms-slide slide-1" data-delay="0.9">    
+        <!-- <div class="slide-pattern"></div> -->
+
+        <img src="images/sliders/slider-1.jpg" data-src="images/sliders/slider-1.jpg" alt="" />
+
+<!-- 		<img class="ms-layer" alt="bootstrap conversion" style="top:150px; right:80px"
          data-offset-x      = "10"
          data-offset-y      = "15"
          data-type          = "image"         
          src="masterslider/blank.gif"
          data-src="images/sliders/master/banner22.png"
-        />
-        
+        /> -->
+   <!-- Animation code hear  -->     
 
 
 
 
 
 	</div>
-<div class="ms-slide slide-2" data-delay="3">
-        <div class="slide-pattern"></div>
-        <img src="js/masterslider/site-img31.jpg" data-src="images/sliders/master/site-img31.jpg" alt="" />	
-		<img class="ms-layer" alt="bootstrap conversion" style="top:150px; left:80px"
+<div class="ms-slide slide-2" data-delay="0.9">
+        <!-- <div class="slide-pattern"></div> -->
+
+        <img src="images/sliders/slider-2.jpg" data-src="images/sliders/slider-2.jpg" alt="" />	
+
+
+<!-- Animation code hear  -->
+<!-- 		<img class="ms-layer" alt="bootstrap conversion" style="top:150px; left:80px"
          data-offset-x      = "10"
          data-offset-y      = "15"
          data-type          = "image"
@@ -29,15 +35,18 @@
          data-ease          = "easeOutQuart"
     	   data-hide-effect	="top(long)"   
          src="masterslider/blank.gif"
-         data-src="images/sliders/master/app_development.png"/>
-        
-               
-	</div> 
+         data-src="images/sliders/master/app_development.png"/>    
+	</div>  -->
     
-     <div class="ms-slide slide-3" data-delay="2">    
-        <div class="slide-pattern"></div>
-        <img src="js/masterslider/site-img31.jpg" data-src="images/sliders/master/site-img31.jpg" alt="" /> 
-         <img class="ms-layer" alt="bootstrap conversion" style="top:150px; right:80px"
+     <div class="ms-slide slide-3" data-delay="0.9">    
+        <!-- <div class="slide-pattern"></div> -->
+        <img src="images/sliders/slider-3.jpg" data-src="images/sliders/slider-3.jpg" alt="" /> 
+
+
+
+
+<!-- Animation code hear  -->
+<!-- <img class="ms-layer" alt="bootstrap conversion" style="top:150px; right:80px"
              data-offset-x      = "10"
              data-offset-y      = "15"
              data-type          = "image"
@@ -47,15 +56,18 @@
         	   data-hide-effect	  ="top(long)"  
              src="masterslider/blank.gif"
              data-src="images/sliders/master/right-respnsive.png"/> 
-
+ -->
 		
 		
               
 	</div>
-   <div class="ms-slide slide-4" data-delay="2">    
-        <div class="slide-pattern"></div>
-        <img src="js/masterslider/site-img31.jpg" data-src="images/sliders/master/site-img31.jpg" alt="" /> 
-       <img class="ms-layer" alt="bootstrap conversion" style="top:150px; left:80px"
+   <div class="ms-slide slide-4" data-delay="0.9">    
+        <!-- <div class="slide-pattern"></div> -->
+        <img src="images/sliders/slider-3.jpg" data-src="images/sliders/slider-3.jpg" alt="" /> 
+
+ 
+        <!--  Animation code hear-->
+<!--        <img class="ms-layer" alt="bootstrap conversion" style="top:150px; left:80px"
            data-offset-x      = "10"
            data-offset-y      = "15"
            data-type          = "image"
@@ -64,7 +76,7 @@
            data-ease          = "easeOutQuart"
            data-hide-effect ="top(long)"  
            src="masterslider/blank.gif"
-           data-src="images/sliders/master/software.png"/>
+           data-src="images/sliders/master/software.png"/> -->
 
    
                
