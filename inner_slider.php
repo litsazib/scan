0d6e7aabffcbd50dde013ce7ab9830e03fca59a0
litsazib <inceptiondj.info@gmail.com>
2016-11-19 22:40:45 +0600

    Inner slider animation add

--- inner_slider.php
+++ inner_slider.php
@@ -1,85 +1,48 @@
 <div class="slidermar sl_color">
 <div class="master-slider " id="masterslider">    
-    <div class="ms-slide slide-1" data-delay="0.9">    
-        <!-- <div class="slide-pattern"></div> -->
-
+    <div class="ms-slide slide-1" data-delay="0.9">
         <img src="images/sliders/slider-1.jpg" data-src="images/sliders/slider-1.jpg" alt="" />
 
-<!-- 		<img class="ms-layer" alt="bootstrap conversion" style="top:150px; right:80px"
+		<img class="ms-layer" alt="bootstrap conversion" style="top:214.536px; right:454.086px"
          data-offset-x      = "10"
          data-offset-y      = "15"
-         data-type          = "image"         
+         data-type          = "image"  
+         data-effect="scale(1.5,1.6)"
+         data-duration="1800"   
+         data-delay="700" 
          src="masterslider/blank.gif"
-         data-src="images/sliders/master/banner22.png"
-        /> -->
-   <!-- Animation code hear  -->     
+         data-src="images/sourservice.png"
+        />
+<!--       <a href="#" class="ms-layer sbut1 white"
+      style="left: 75px; top: 430px;"
+      data-type="text"
+      data-delay="700"
+      data-ease="easeOutExpo"
+      data-duration="1800"
+      data-effect="scale(1.5,1.6)">
+      Details            
+    </a>  -->   
+<img class="ms-layer" alt="bootstrap conversion" style="top:328.536px; left:217.086px"
+         data-offset-x      = "10"
+         data-offset-y      = "15"
+         data-type          = "image"
+         data-effect    ="top(long,false)"
+         data-duration      = "1800"
+         data-ease          = "easeOutQuart"
+         data-effect="scale(1.5,1.6)"         
+         data-src="images/mobile.png"/> 
 
 
 
 
 
 	</div>
-<div class="ms-slide slide-2" data-delay="0.9">
-        <!-- <div class="slide-pattern"></div> -->
 
-        <img src="images/sliders/slider-2.jpg" data-src="images/sliders/slider-2.jpg" alt="" />	
-
-
-<!-- Animation code hear  -->
-<!-- 		<img class="ms-layer" alt="bootstrap conversion" style="top:150px; left:80px"
-         data-offset-x      = "10"
-         data-offset-y      = "15"
-         data-type          = "image"
-         data-effect		="top(long,false)"
-         data-duration      = "1800"
-         data-ease          = "easeOutQuart"
-    	   data-hide-effect	="top(long)"   
-         src="masterslider/blank.gif"
-         data-src="images/sliders/master/app_development.png"/>    
-	</div>  -->
-    
-     <div class="ms-slide slide-3" data-delay="0.9">    
-        <!-- <div class="slide-pattern"></div> -->
-        <img src="images/sliders/slider-3.jpg" data-src="images/sliders/slider-3.jpg" alt="" /> 
-
-
-
-
-<!-- Animation code hear  -->
-<!-- <img class="ms-layer" alt="bootstrap conversion" style="top:150px; right:80px"
-             data-offset-x      = "10"
-             data-offset-y      = "15"
-             data-type          = "image"
-             data-effect		    ="top(long,false)"
-             data-duration      = "1800"
-             data-ease          = "easeOutQuart"
-        	   data-hide-effect	  ="top(long)"  
-             src="masterslider/blank.gif"
-             data-src="images/sliders/master/right-respnsive.png"/> 
- -->
+   
 		
 		
               
 	</div>
-   <div class="ms-slide slide-4" data-delay="0.9">    
-        <!-- <div class="slide-pattern"></div> -->
-        <img src="images/sliders/slider-3.jpg" data-src="images/sliders/slider-3.jpg" alt="" /> 
-
- 
-        <!--  Animation code hear-->
-<!--        <img class="ms-layer" alt="bootstrap conversion" style="top:150px; left:80px"
-           data-offset-x      = "10"
-           data-offset-y      = "15"
-           data-type          = "image"
-           data-effect        ="top(long,false)"
-           data-duration      = "1800"
-           data-ease          = "easeOutQuart"
-           data-hide-effect ="top(long)"  
-           src="masterslider/blank.gif"
-           data-src="images/sliders/master/software.png"/> -->
-
    
-               
-  </div>
 </div>
 </div>
